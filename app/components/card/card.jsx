57cdb94dd33ccdd5bd0786b2c0cd323732e1adf6
Rai Pocius <pocius.raimondas@gmail.com>
2024-07-08 20:45:12 -0500

    Start styling card component

--- app/components/card/card.jsx
+++ app/components/card/card.jsx
@@ -1,15 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
 import styles from './card.module.css';
+import PostHeader from '../post_header/post_header.jsx'
 
 export default function Card() {
     return(
-        <>
-            
-        </>
+        <div className={styles.card}>
+            <PostHeader />
+            <div className={styles.post}>
+                <h2>Post Name</h2>
+                <p>Post content Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem debitis voluptatem eius aspernatur mollitia sequi, consequuntur ipsum cum architecto obcaecati? Impedit fugit modi sunt dignissimos esse obcaecati autem quidem aut!</p>
+            </div>
+            <div className={styles.footer}>
+                <button>Comments</button>
+                <button>Share</button>
+            </div>
+        </div>
     )
 };
-
-
-// IMG      SUBREDDIT       TIMESTAMP
-// H2
-// POST (<p> and/or IMG)
-// ON/OFF COMMENTS      SHARE BUTTON?
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import styles from "./page.module.css";
 import Header from "./components/header/header.jsx";
+import Card from "./components/card/card.jsx"
 
 export default function Home() {
   return (
     <>
       <Header />
       <main className={styles.main}>
+        <Card />
         <p>Rest of the page</p>
       </main>
     </>
